80eb43a7258c1afa0289693f0919bde1dbf29b6a
Avi <patelavi443@gmail.com>
2022-05-02 21:00:24 +0530

    Write a program to read the price of an item in decimal form (  like 15.95 ) and print the output in paisa

---
+++ chap-2/p-3.c
@@ -0,0 +1,25 @@
+/* Write a program to read the price of an item in decimal form 
+(  like 15.95 ) and print the output in paisa */
+
+#include<stdio.h>
+
+#include<conio.h>
+
+void main()
+
+{
+
+int b;
+
+float a;
+
+a=15.95;
+
+b=100*a;
+
+printf("%d",b);
+
+getch();
+
+}
+
